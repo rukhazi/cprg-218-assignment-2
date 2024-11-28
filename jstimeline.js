27934ab4fabe390cf84d8fcfeca023e2b3ba83c3
andrea <ramora.drea@gmail.com>
2024-11-27 22:45:36 -0700

    meow

---
+++ jstimeline.js
@@ -0,0 +1,30 @@
+introbutton.addEventListener("click",function(){
+    console.log("introclicked");
+    document.getElementById("paragraphtitle").innerHTML="Introduction";
+    document.getElementById("paragraph").innerHTML="Nami is a fashionable, orange-haired lady who dreams of navigating and creating a map of the world. She is the Navigator for the Straw Hat Pirates which allowed the crew to safely traverse through many dangerous waters. Originally, she was orphaned due to a war however was taken in by an ex-marine soldier; Bell-mere.";
+    document.getElementById("image").src="images/Nami_intro.webp";
+
+})
+
+button2.addEventListener("click",function(){
+    console.log("2clicked");
+    document.getElementById("paragraphtitle").innerHTML="East Blue";
+    document.getElementById("paragraph").innerHTML="During the “East Blue Arc” (Episode 31-61) Nami is first introduced to the infamous Straw Hat pirates which, at the time, only had 3 members; Luffy, Zoro, and Sanji. During this arc, we are shown the reason Nami is nicknamed the “Cat Burglar”. In order to make money, she stole from the rich; a play on “Robin Hood”, however her circumstances were a tad different. Her small village was enslaved by pirates and the only way to set them free was if Nami stole enough money for them.";
+    document.getElementById("image").src="images/Nami_EastBlue.png";
+})
+
+button3.addEventListener("click",function(){
+    console.log("3clicked");
+    document.getElementById("paragraphtitle").innerHTML="Whole Cake";
+    document.getElementById("paragraph").innerHTML="In Whole Cake, Nami is tasked in defeating one of the 3 Sweet Commanders (The strongest defense in the island); Cracker. This victory signifies the massive improvement of Nami as she was known as one of the weaker fighters of the Straw Hats. She also displays her kleptomaniac side when she steals a dangerous weapon from Big Mom, a highly feared “Emperor of the Sea”, therefore inflicting massive damage.";
+    document.getElementById("image").src="images/Nami_WholeCake.png";
+
+})
+
+button4.addEventListener("click",function(){
+    console.log("4clicked");
+    document.getElementById("paragraphtitle").innerHTML="Wano";
+    document.getElementById("paragraph").innerHTML="Wano Island crawls with dangerous pirates and the Beasts Pirates led by another Emperor of the Sea, Kaido. Despite not being a front line combatant, Nami stands up against another strong opponent, Ulti, who is a Tobiroppo. This arc also shines a light on her loyalty towards her captain, Luffy.";
+    document.getElementById("image").src="images/Nami_Wano.png";
+
+})
